0382eeb6409cc863abc88d87f1ac9d6b371eb650
Rusan Alexandru <rusannalexandru@gmail.com>
2024-12-16 17:47:14 +0200

    Update endless pagination / scroll

--- wwwroot/app.js
+++ wwwroot/app.js
@@ -1,7 +1,6 @@
 ﻿function initializeScrollEvent(dotNetReference) {
-    const container = document.getElementById("cryptoTableContainer");
-    container.addEventListener("scroll", () => {
-        if (container.scrollHeight - container.scrollTop <= container.clientHeight + 50) {
+    window.addEventListener("scroll", () => {
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 50) {
             dotNetReference.invokeMethodAsync("OnScrollToBottom");
         }
     });
